ca13bc196f49fdaf736f887eb88f91079dccd466
qcyap <qcyap.2011@sis.smu.edu.sg>
2014-11-08 20:51:41 +0800

    - Added UXArmy code

---
+++ js/uxarmy.js
@@ -0,0 +1,6 @@
+var _uxa = _uxa || [];
+(function() {
+    var uxa = document.createElement("script"); uxa.type = "text/javascript"; uxa.async = true;
+    uxa.src = "//www.uxarmy.com/test/js";
+    document.getElementsByTagName("head")[0].appendChild(uxa);
+})();
