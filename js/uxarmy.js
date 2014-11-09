39e972a52b3fcc21a111f7542e73067965bd041c
qcyap <qcyap.2011@sis.smu.edu.sg>
2014-11-09 11:21:55 +0800

    - Removed UXArmy code

--- js/uxarmy.js
+++
@@ -1,6 +0,0 @@
-var _uxa = _uxa || [];
-(function() {
-    var uxa = document.createElement("script"); uxa.type = "text/javascript"; uxa.async = true;
-    uxa.src = "//www.uxarmy.com/test/js";
-    document.getElementsByTagName("head")[0].appendChild(uxa);
-})();
